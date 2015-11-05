94c23be2ce1b25fea4c9d41dce9a485d01961450
Choi-Hyunwoo <cho7673@gmail.com>
2015-11-05 23:57:41 +0900

    기초정보입력 완료

--- app/src/main/java/aftercoffee/org/nonsmoking365/activity/main/StateFragment.java
+++ app/src/main/java/aftercoffee/org/nonsmoking365/activity/main/StateFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import aftercoffee.org.nonsmoking365.R;
 import aftercoffee.org.nonsmoking365.activity.board.BoardActivity;
@@ -19,6 +20,7 @@ import aftercoffee.org.nonsmoking365.activity.CentersActivity;
  */
 public class StateFragment extends Fragment {
 
+    TextView gradeView, timeView, savedMoneyView, statusView;
 
     public StateFragment() {
         // Required empty public constructor
@@ -28,9 +30,19 @@ public class StateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_state, container, false);
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_state, container, false);
+
+        // Initialize view
+        gradeView = (TextView)view.findViewById(R.id.text_gradeView);
+        timeView = (TextView)view.findViewById(R.id.text_timeView);
+        savedMoneyView = (TextView)view.findViewById(R.id.text_savedmoneyView);
+        statusView = (TextView)view.findViewById(R.id.text_status);
+
+        // 회원 등급
+        gradeView.setText("일반 회원");
+
+
 
         Button btn = (Button)view.findViewById(R.id.btn_warning);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +75,5 @@ public class StateFragment extends Fragment {
 
         return view;
     }
-
 
 }
