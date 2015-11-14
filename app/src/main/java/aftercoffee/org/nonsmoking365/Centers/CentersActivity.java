fe90ae5ba06ab3ee39b9f8a331031e2aaaef0177
Choi-Hyunwoo <cho7673@gmail.com>
2015-11-14 22:45:48 +0900

    보건소 리스트뷰 리스너 구현완료

--- app/src/main/java/aftercoffee/org/nonsmoking365/Centers/CentersActivity.java
+++ app/src/main/java/aftercoffee/org/nonsmoking365/Centers/CentersActivity.java
@@ -1,25 +1,14 @@
 package aftercoffee.org.nonsmoking365.Centers;
 
-import android.content.Context;
 import android.content.Intent;
-import android.location.LocationManager;
-import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.skp.Tmap.TMapData;
-import com.skp.Tmap.TMapPOIItem;
-
-import java.net.URI;
-import java.util.ArrayList;
 
 import aftercoffee.org.nonsmoking365.Manager.NetworkManager;
 import aftercoffee.org.nonsmoking365.R;
