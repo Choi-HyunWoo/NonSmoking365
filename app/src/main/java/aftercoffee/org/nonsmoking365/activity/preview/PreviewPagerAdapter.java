b4cfedf7fe41c33890c657ef87b7e4e645d86fb3
Choi-Hyunwoo <cho7673@gmail.com>
2015-11-30 01:42:56 +0900

    프리뷰

--- app/src/main/java/aftercoffee/org/nonsmoking365/activity/preview/PreviewPagerAdapter.java
+++ app/src/main/java/aftercoffee/org/nonsmoking365/activity/preview/PreviewPagerAdapter.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import aftercoffee.org.nonsmoking365.R;
 
 /**
  * Created by HYUNWOO on 2015-11-29.
@@ -16,6 +21,27 @@ public class PreviewPagerAdapter extends FragmentPagerAdapter {
 
     public PreviewPagerAdapter (FragmentManager fm) {
         super(fm);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view;
+        switch (position) {
+            case 0 :
+                view = LayoutInflater.from(container.getContext()).inflate(R.layout.view_tutorial_01, container, false);
+                break;
+            case 1 :
+                view = LayoutInflater.from(container.getContext()).inflate(R.layout.view_tutorial_02, container, false);
+                break;
+            case 2 :
+                view = LayoutInflater.from(container.getContext()).inflate(R.layout.view_tutorial_03, container, false);
+                break;
+            default:
+                view = LayoutInflater.from(container.getContext()).inflate(R.layout.view_tutorial_01, container, false);
+                break;
+        }
+        container.addView(view);
+        return view;
     }
 
     @Override
--- app/src/main/java/aftercoffee/org/nonsmoking365/activity/preview/PreviewpageFragment.java
+++ app/src/main/java/aftercoffee/org/nonsmoking365/activity/preview/PreviewpageFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import aftercoffee.org.nonsmoking365.R;
 
@@ -19,6 +20,7 @@ public class PreviewpageFragment extends Fragment {
         // Required empty public constructor
     }
 
+    ImageView tutorialImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
